fe2fe2824669da6868e0f08730a19b43a852ab2f
WiiruxCruz <hialf.92@gmail.com>
2022-01-28 13:42:52 -0600

    Creación de modulos y carga por lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: 'template',
+    loadChildren: () => import('./template/template.module').then( m => m.TemplateModule )
+  },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule )
+  }
 ];
 
 @NgModule({
