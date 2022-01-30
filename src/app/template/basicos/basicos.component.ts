a6055fee731812823ac65c9c6e1b9773c9bb745d
WiiruxCruz <hialf.92@gmail.com>
2022-01-29 20:07:01 -0600

    Implementación de view child

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,14 +9,36 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  //@ViewChild('miFormulario', {static: false}) miFormulario!: NgForm;
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar( miFormulario: NgForm ) {
-    console.log( miFormulario );
-    console.log( miFormulario.value );
+  nombreValido(): boolean {
+    //el principal problema era que tenía producto con p mayusculas en HTML y la buscaba con minuscula en TS
+    /*
+    Esta versión no aplica, encontrado en udemy
+    Salta error en .producto? dentro de la función nombreValido
+    console.log(this.miFormulario?.controls['producto'].invalid
+    && this.miFormulario?.controls['producto'].touched);
+    */
+    console.log(this.miFormulario?.controls.producto?.invalid
+      && this.miFormulario?.controls.producto?.touched);
+
+    return this.miFormulario?.controls.producto?.invalid
+    && this.miFormulario?.controls.producto?.touched;
+
+    //return this.miFormulario?.controls['producto']?.invalid
+    //&& this.miFormulario?.controls['producto']?.touched;
+  }
+
+  //guardar( miFormulario: NgForm ) {
+    //console.log( miFormulario );
+  guardar() {
+    console.log( this.miFormulario );
   }
 
 }
