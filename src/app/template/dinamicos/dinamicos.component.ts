00ff2295ce08670352bd4ec1039a8e6fd5bb5e69
WiiruxCruz <hialf.92@gmail.com>
2022-01-29 22:52:47 -0600

    Agregar elementos de manera dinámica

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  persona: Persona = {
+    nombre: 'Alfredo',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Pokemon' }
+    ]
 
-  ngOnInit(): void {
+  }
+
+
+  guardar() {
+    console.log('Formulario posteado');
   }
 
 }
