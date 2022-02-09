a7be3bde96f47e06703a296df710b6996118d527
WiiruxCruz <hialf.92@gmail.com>
2022-02-09 00:37:50 -0600

    Validar nuevo campo en forms reactivos dinamicos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  miFormulario: FormGroup = this.fb.group ({
+    nombre: [ '', [Validators.required, Validators.minLength(3)] ]
+  });
+
+  campoEsValido( campo: string) {
+    return this.miFormulario.controls[campo].errors
+    && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+
+    if( this.miFormulario.invalid ){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    //imprimir valor del formulario solo si es valido
+    console.log(this.miFormulario.value);
   }
 
 }
