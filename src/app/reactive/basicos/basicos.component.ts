ab9418a7be556dc96af3c04a54f634a841314682
WiiruxCruz <hialf.92@gmail.com>
2022-02-09 00:23:28 -0600

    Submit del formulario

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,18 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit{
+
+  constructor( private fb: FormBuilder ) { }
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'RTX 4800ti',
+      precio: 1600
+    });
+  }
+
+
   /*
   miFormulario: FormGroup = new FormGroup({
     nombre: new FormControl('RTX 4800'),
@@ -22,11 +33,19 @@ export class BasicosComponent {
     existencias: [ 0 , [Validators.required, Validators.min(0)]]
   });
 
-  constructor( private fb: FormBuilder ) { }
-
   campoEsValido( campo: string) {
     return this.miFormulario.controls[campo].errors
     && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+    if( this.miFormulario.invalid ){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 
 }
