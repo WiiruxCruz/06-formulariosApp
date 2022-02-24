0341d140178bf0fc7336e2a3a28b900aefc613f8
WiiruxCruz <hialf.92@gmail.com>
2022-02-24 01:17:34 -0600

    Carga perezosa del modulo Auth

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -47,4 +47,16 @@ export class SidemenuComponent {
       ruta: './reactive/switches'
     }
   ];
+
+
+  authMenu: MenuItem[] = [
+    {
+      texto: 'Registro',
+      ruta: './auth/registro'
+    },
+    {
+      texto: 'Login',
+      ruta: './auth/login'
+    }
+  ];
 }
