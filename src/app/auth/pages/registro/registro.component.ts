60b376b5301c5fb85c22c1a7628b10b149b1e38c
WiiruxCruz <hialf.92@gmail.com>
2022-03-01 00:01:26 -0600

    Validación de contraseñas iguales

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -15,7 +15,11 @@ export class RegistroComponent implements OnInit {
 	miFormulario: FormGroup = this.fb.group({
 		nombre: ['', [ Validators.required, Validators.pattern( this.validatorService.nombreApellidoPattern ) ]],
 		email: ['', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ]],
-		username: ['', [ Validators.required, this.validatorService.noPuedeSerStrider ]]
+		username: ['', [ Validators.required ] ],
+		password: ['', [ Validators.required, Validators.minLength(6) ] ],
+		password2: ['', [ Validators.required ] ]
+	}, {
+		validators: [ this.validatorService.camposIguales('password', 'password2') ]
 	});
 
 	constructor(
